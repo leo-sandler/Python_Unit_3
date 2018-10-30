25ad11eb5e9b4b5692598f2a920ff092a699bcff
Leo_Sandler <leo.sandler@greenwoodcollege.com>
2018-10-30 09:39:55 -0400

    Completed task 2  of lesson 3.1.5

--- Defining Functions Docs/Functions Tasks.py
+++ Defining Functions Docs/Functions Tasks.py
@@ -10,3 +10,21 @@ def product_function():
 
 
 print(product_function())
+
+# Task 2
+def comp_nums():
+    num_1 = int(input("Enter the first #: "))
+    num_2 = int(input("Enter the second  #: "))
+    comp_checking(num_1, num_2)
+
+def comp_checking(num_1, num_2):
+    if num_1 == num_2:
+        print("True")
+    else:
+        print("False")
+
+
+print(comp_nums())
+# Not sure why this states none after running, but it works properly.
+
+# Task 3

0213e243d6302dab5dcaba98f24eebf50f1f6517
Leo_Sandler <leo.sandler@greenwoodcollege.com>
2018-10-30 09:34:27 -0400

    Completed task 1 of lesson 3.1.5

---
+++ Defining Functions Docs/Functions Tasks.py
@@ -0,0 +1,12 @@
+# Task 1
+
+
+def product_function():
+    num_1 = int(input("Enter the first #: "))
+    num_2 = int(input("Enter the second  #: "))
+    prod = (num_1*num_2)
+
+    return prod
+
+
+print(product_function())

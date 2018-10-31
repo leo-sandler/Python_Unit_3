ca927f868fd19104504c405f1aea46ea623637cc
Leo_Sandler <leo.sandler@greenwoodcollege.com>
2018-10-31 13:38:25 -0400

    Completed the examples

--- Defining Functions Docs/Functions Examples.py
+++ Defining Functions Docs/Functions Examples.py
@@ -1,4 +1,8 @@
+import time
+import random
 # Calling a function: Syntax: function_name(list_of_params), or function_name() with empty brackets with no params
+
+
 def print_halloween():
     print("Happy Halloween!")
 
@@ -14,9 +18,9 @@ def print_halloween_1(times):
 print_halloween_1(3)
 
 
-def print_halloween_2(inner, outer):
+def print_halloween_2(inny, outer):
     for x in range(outer):
-        for y in range(inner):
+        for y in range(inny):
             print("Happy Halloween!")
 
 
@@ -25,10 +29,10 @@ print_halloween_2(3, 3)
 # This is a nested for loop
 
 
-def print_halloween_3(inner, outer):
+def print_halloween_3(inne, outer):
     for x in range(outer):
         print("Test")
-        for y in range(inner):
+        for y in range(inne):
             print("Happy Halloween!")
 
 
@@ -36,10 +40,10 @@ print_halloween_3(3, 3)
 # This will print 3 tests, and 9 happy halloweens
 
 
-def print_halloween_4(inner):
-    if inner > 7:
+def print_halloween_4(innar):
+    if innar > 7:
         return True
-    elif inner == 7:
+    elif innar == 7:
         return False
     else:
         return "Boo"
@@ -54,3 +58,43 @@ print(print_halloween_4(user_num))
 
 print(print_halloween_4(int(input("Gimme a Number: "))))
 # This notation does the exact same thing
+
+# Spooky Function Tasks 1
+# Create a function that has no parameters. It will use time.sleep(x) and random to, at a random time print "BOO!"
+
+
+def sleepy_func():
+    print("wait...")
+    time.sleep(random.randint(1, 5))
+    print("BOO!")
+
+
+sleepy_func()
+
+# Spooky Functions Task 2
+# Create a function that returns trick or treat randomly
+inner = random.randint(1, 2)
+
+
+def trick_or_treat(inner):
+    print("Wait 2 seconds...")
+    time.sleep(2)
+    if inner == 1:
+        return "Trick"
+    elif inner == 2:
+        return "Treat"
+
+
+print(trick_or_treat(inner))
+
+# Spooky Task 3:
+# Create a function that has 1 parameter, a string, adds "Spooky" to the beginning of that string, and returns it.
+user_str = str(input("Enter Your Name: "))
+
+
+def spooky_ad(user_str):
+    spook = "Spooky " + user_str
+    return spook
+
+
+print("Hello" + spooky_ad(user_str))
